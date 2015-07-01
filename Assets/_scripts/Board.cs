d9a522eda313625cc8e5fb883a8d76b0eb301550
Charlie Bevis <charliebevis@users.noreply.github.com>
2015-06-30 22:34:08 -0700

    get rid of the whole notion of blenderUnit in the board script

--- Assets/_scripts/Board.cs
+++ Assets/_scripts/Board.cs
@@ -5,19 +5,19 @@ namespace Application
 {
 	public class Board
 	{
-		private float blenderUnit = 1.74f;
+		private float blenderUnit = 1f;
 
 		public Board ()
 		{
 		}
 
 		public Vector3 positionForIndex(int row, int column) {
-			float evenRowAdjustment = column % 2 == 0 ? blenderUnit/2 : 0;
-			float row3Adjustment = column == 2 ? -blenderUnit : 0;
+			float evenRowAdjustment = column % 2 == 0 ? 1f/2 : 0;
+			float row3Adjustment = column == 2 ? -1f : 0;
 
 			return new Vector3(row*blenderUnit + evenRowAdjustment + row3Adjustment,
 			                   0,
-			                   column*blenderUnit * 0.87f);
+			                   column * 0.87f);
 		}
 	}
 }
