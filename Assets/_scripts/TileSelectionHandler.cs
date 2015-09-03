c703cea3e06475dc770256e1cff0f32864ed56f9
Charlie Bevis <charliebevis@users.noreply.github.com>
2015-09-02 20:59:39 -0700

    take the selection color handling out of the update() method to make it performant

--- Assets/_scripts/TileSelectionHandler.cs
+++ Assets/_scripts/TileSelectionHandler.cs
@@ -8,22 +8,15 @@ public class TileSelectionHandler : MonoBehaviour {
 
 	void OnMouseUp() {
 		if(selectedObjects.Contains (gameObject)){
+			gameObject.renderer.material.SetColor ("_Color", defaultColor);
 			selectedObjects.Remove(gameObject);
 		}else{
-			selectedObjects.Add (gameObject);
+			gameObject.renderer.material.SetColor ("_Color", Color.green);
+			selectedObjects.Add(gameObject);
 		}
 	}
 	
 	void Start () {
 		defaultColor = new Color(1.0f, 1.0f, 1.0f, 0.5f);
-	}
-	
-	void Update () {
-		if (selectedObjects.Contains (gameObject)) {
-			Debug.Log (gameObject.renderer.material.GetColor("_Color"));
-			gameObject.renderer.material.SetColor ("_Color", Color.green);
-		} else {
-			gameObject.renderer.material.SetColor ("_Color", defaultColor);
-		}
 	}
 }
